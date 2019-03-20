fde42717b6ae393e4e2839310803dd92144fe940
hafid benamar <cyberhafid@gmail.com>
2019-03-20 11:25:16 +0100

    premier

---
+++ Node.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const chalk = require('chalk');
+
+const log = console.log;
+log(chalk.blue('dan') + ' steph' + chalk.red('radoudou')+ chalk.green('nicolas'));
+
+
+
+
+//const server = http.createServer((request, response) => {
+
+ //sur chrome
+   // response.writeHead(200);
+   // response.end('Hello World /n doudou');
+    
+//});
+//server.listen(3000);
+//console.log('Adresse du serveur: http://localhost:3000');
+
