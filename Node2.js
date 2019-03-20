17d835fb5bc35e307f17feca0a37b26b216a1c69
hafid benamar <cyberhafid@gmail.com>
2019-03-20 11:36:42 +0100

    ok c'est le bon

--- Node2.js
+++ Node2.js
@@ -1,19 +1,23 @@
 const http = require('http');
 const chalk = require('chalk');
 
-const log = console.log;
-log(chalk.blue('dan') + ' steph' + chalk.red('radoudou')+ chalk.green('nicolas'));
 
 
-
-
-//const server = http.createServer((request, response) => {
+const server = http.createServer((request, response) => {
 
  //sur chrome
-   // response.writeHead(200);
-   // response.end('Hello World /n doudou');
+  response.writeHead(200);
+
+ // const log = console.log;
+ const paru= log(chalk.blue('dan') + ' steph' + chalk.red('radoudou')+ chalk.green('nicolas'));
+
+   response.end('Hello World /n doudou');
     
-//});
-//server.listen(3000);
-//console.log('Adresse du serveur: http://localhost:3000');
+});
+server.listen(3000);
+console.log('Adresse du serveur: http://localhost:3000');
+
+const log = console.log;
+const paru= log(chalk.blue('dan  ') + ' steph  ' + chalk.red('radoudou  ')+ chalk.green('nicolas  '));
+console.log(paru);
 
