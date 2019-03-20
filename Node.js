29ccc8d6863abd209241e0613d50bd8143e32465
hafid benamar <cyberhafid@gmail.com>
2019-03-20 11:42:33 +0100

    ok final

--- Node.js
+++ Node.js
@@ -6,18 +6,14 @@ const chalk = require('chalk');
 const server = http.createServer((request, response) => {
 
  //sur chrome
-  response.writeHead(200);
-
- // const log = console.log;
- const paru= log(chalk.blue('dan') + ' steph' + chalk.red('radoudou')+ chalk.green('nicolas'));
-
-   response.end('Hello World /n doudou');
+     response.writeHead(200);
+    response.end('Hello World /n doudou');
     
 });
 server.listen(3000);
 console.log('Adresse du serveur: http://localhost:3000');
 
-//const log = console.log;
+
 console.log(chalk.blue('dan  ') + ' steph  ' + chalk.red('radoudou  ')+ chalk.green('nicolas  '));
-//console.log(paru);
+
 
